70a4a50eb6d7731d96ef6faf44ced27afff8fee9
anakonder <120507138+anakonder@users.noreply.github.com>
2023-04-30 03:02:23 +0300

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form')
 
+
+
+form.addEventListener('submit', handlerSubmit);
+
+function handlerSubmit(event) { 
+    event.preventDefault();
+    const { elements: { email, password } } = event.currentTarget;
+    if (email.value === '' || password.value === '') {return alert('Please fill in all the fields!') }
+    console.log(this.elements.email.value)
+    console.log(this.elements.password.value)  
+    form.reset();
+}
